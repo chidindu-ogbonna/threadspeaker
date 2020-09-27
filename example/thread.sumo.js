e1a7f7d7dbed94d4ef9435b67051c6393d4b337d
Chidindu Promise Ogbonna <promise.bones@gmail.com>
2020-09-27 21:35:11 +0100

    update: Include example files

---
+++ example/thread.sumo.js
@@ -0,0 +1,69 @@
+const thread = [
+  {
+    created_at: "2020-09-08T18:20:08.000Z",
+    media: [],
+    urls: [],
+    id_str: "1303397727288475649",
+    full_text:
+      "5 tips to follow when you're building a new product:\n\n1) See if you can get customers to give you money BEFORE you build ANYTHING.\n\n2) Build something for yourself, and use the product regularly as you're building it.",
+    in_reply_to_status_id_str: null,
+    in_reply_to_user_id_str: null,
+    in_reply_to_screen_name: null,
+    is_quote_status: false,
+  },
+  {
+    created_at: "2020-09-08T18:20:09.000Z",
+    media: [],
+    urls: [],
+    id_str: "1303397728320327680",
+    full_text:
+      "3) Set artificial limitations. Limit the amount of time and money you spend on building a service or product.\n\n4) Be careful what metric of success you pick. The BEST one is one that aligns customer success and business success.",
+    in_reply_to_status_id_str: "1303397727288475649",
+    in_reply_to_user_id_str: "13737",
+    in_reply_to_screen_name: "noahkagan",
+    is_quote_status: false,
+  },
+  {
+    created_at: "2020-09-08T18:20:09.000Z",
+    media: [],
+    urls: [],
+    id_str: "1303397729138143233",
+    full_text:
+      "5) Date your business idea but marry your vision. Don't become too attached to what you're building. Iterate on your product fast. Remove features that don’t help you move towards the vision.",
+    in_reply_to_status_id_str: "1303397728320327680",
+    in_reply_to_user_id_str: "13737",
+    in_reply_to_screen_name: "noahkagan",
+    is_quote_status: false,
+  },
+  {
+    created_at: "2020-09-08T18:20:09.000Z",
+    media: [],
+    urls: ["https://youtu.be/MMUx8puoCIc"],
+    id_str: "1303397730056720384",
+    full_text:
+      "At the beginning of the year, we launched a new product (@hauldrop) that reached nearly 1 million visitors in 9 months. We failed a lot and learned a lot in the process.\n\nIf you want to see what the building process was like, check out my last video: https://t.co/NCPeQeZJB0",
+    in_reply_to_status_id_str: "1303397729138143233",
+    in_reply_to_user_id_str: "13737",
+    in_reply_to_screen_name: "noahkagan",
+    is_quote_status: false,
+  },
+];
+
+const author = {
+  id_str: "13737",
+  name: "Noah Kagan",
+  screen_name: "noahkagan",
+  location: "Austin, TX",
+  url: "https://t.co/2LCjbM8NpT",
+  verified: true,
+  profile_image_url_https:
+    "https://pbs.twimg.com/profile_images/874640458240184320/53G2ALd5_normal.jpg",
+  followers_count: 67283,
+  friends_count: 161,
+  protected: false,
+  created_at: "2006-11-22T01:38:27.000Z",
+  description:
+    "I’m the 🌮 loving Chief Sumo at @appsumo. I was employee #30 at Facebook. I teach everything I know about marketing & business on my YouTube channel👇",
+  favourites_count: 11543,
+  statuses_count: 19125,
+};
